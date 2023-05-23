9fcf78bfdf18b68e9d2b7f79ab7b9d61db9c2ef3
Eldor Alijonov <eldoralijonov2001@gmail.com>
2023-05-23 15:58:50 +0500

    ad navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-import { Main,Login, Register } from './view';
+import { Main, Login, Register } from './view';
+import { Navbar } from "./components";
 import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,38 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <div>
-            <h1>Navbar</h1>
+        <div className="position-relative fixed-top">
+            <nav className="navbar navbar-expand-lg bg-white-tertiary border-bottom py-3">
+                <div className="container">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Features</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled">Disabled</a>
+                            </li>
+                        </ul>
+                        <div className="ms-auto d-flex">
+                            <a href="#" className="btn btn-primary me-4 px-4">Kirish</a>
+                           <select>
+                            <option value="1"></option>
+                           </select>
+                        </div>
+                        
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
